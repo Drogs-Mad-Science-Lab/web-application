5e7323e2be6e709483711e98ad76af85461a68a4
Drew Thomas <81405547+drew-marsfarm@users.noreply.github.com>
2023-08-13 00:44:16 -0500

    updated paths

--- services/server-start.sh
+++ services/server-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /path/to/frontend && npm start & # Run frontend in the background
-cd /path/to/backend && npm start &  # Run backend in the background
+cd /home/ubuntu/web-application/frontend && npm start & # Run frontend in the background
+cd /home/ubuntu/web-application/backend && npm start &  # Run backend in the background
